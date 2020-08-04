09948f5426712aa036da2b0bbfa1057c6bf98545
AG <agruszczewski@post.pl>
2020-08-04 14:12:41 +0200

    in memory users

---
+++ src/main/java/ckj46/com/jtc/JtcSecurityConfiguration.java
@@ -0,0 +1,20 @@
+package ckj46.com.jtc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class JtcSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("ag")
+                .password("{bcrypt}$2y$06$day85wGo9n/ob8a/1HdQyuaMYi7J5upoT2sFeuHUtI.eCVFlN5rOq")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("{bcrypt}$2y$06$day85wGo9n/ob8a/1HdQyuaMYi7J5upoT2sFeuHUtI.eCVFlN5rOq")
+                .roles("ADMIN");
+    }
+}
